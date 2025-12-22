2b1f469e9615f293fa969a58b35e19b3ac7a29df
Your Name <you@example.com>
2025-12-23 00:22:20 +0530

    javascript page added externally, previously was inside html page

--- PortfolioWebsite/js/script.js
+++ PortfolioWebsite/js/script.js
@@ -1,1 +1,13 @@
-console.log("Portfolio Loaded Successfully!");
+//Setup and start animation!
+document.addEventListener("DOMContentLoaded", () => {
+    new Typed("#element", {
+        strings: [
+            "Full Stack Developer",
+            "Python Developer",
+            "System Administrator (AWS/Linux)"
+        ],
+        typeSpeed: 50,
+        backSpeed: 30,
+        loop: true
+    });
+});
